819820b599d9468632fb13b6867bb23a515583de
Ahdaan Hashim <ahdaanhashim67@gmail.com>
2024-08-21 00:53:29 +0530

    Implement login and sign-up functionality for admin, employer, and jobseeker

---
+++ auth_service/pkg/helper/employer_helper.go
@@ -0,0 +1,66 @@
+package helper
+
+import (
+	"Auth/pkg/utils/models"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+type authCustomClaimsEmployer struct {
+	Id           uint   `json:"id"`
+	Company_name string `json:"company_name"`
+	Email        string `json:"email"`
+	jwt.StandardClaims
+}
+
+func GenerateTokenEmployer(employer models.EmployerDetailsResponse) (string, error) {
+	claims := &authCustomClaimsEmployer{
+		Id:           employer.ID,
+		Company_name: employer.CompanyName,
+		Email:        employer.ContactEmail,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 48).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte("123456789"))
+	if err != nil {
+		fmt.Println("Error is", err)
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func ValidateTokenEmployer(tokenString string) (*authCustomClaimsEmployer, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &authCustomClaimsEmployer{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("123456789"), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*authCustomClaimsEmployer); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, fmt.Errorf("invalid token")
+}
+
+func GenerateVideoCallKey(userID, oppositeUser int) (string, error) {
+	currentTime := strconv.FormatInt(time.Now().UnixNano(), 10)
+	key := strconv.Itoa(userID) + "_" + strconv.Itoa(oppositeUser) + "_" + currentTime
+	hash := md5.Sum([]byte(key))
+	keyString := hex.EncodeToString(hash[:])
+
+	return keyString, nil
+}
